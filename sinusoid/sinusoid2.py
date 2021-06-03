64c637a26fdbeac2ecfb3dd01bd24cdd19a412b8
Alex Gu <gua@mit.edu>
2021-06-03 11:32:30 -0400

    cleaned up code for sinusoid regression

--- sinusoid/sinusoid2.py
+++ sinusoid/sinusoid2.py
@@ -1,21 +1,11 @@
-import higher
+import pickle
+from matplotlib import pyplot as plt
 import numpy as np
-import hypergrad as hg
-from generate_task import Sine
-
-# amplitude: [0.1, 5]
-# phase: [0, pi]
-# A * sin(x-phase)
-
-# data uniformly in [-5, 5]
 np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
-
 import torch
 import torch.nn as nn
 
-train_tasks = 20
-test_tasks = 10
-sine_task = Sine(train_tasks, test_tasks)
+from generate_task import Sine
 
 def simplex_projection(s):
     """Projection onto the unit simplex."""
@@ -26,32 +16,36 @@ def simplex_projection(s):
     return np.maximum(s-theta, 0)
 
 class Learner():
-    def __init__(self, sine, alpha=0.003, beta=0.005, gamma=0.003, update_lambdas=False):
-        self.gamma = gamma
-        self.update_lambdas = update_lambdas
-        self.task = sine
-        self.embedding_dim = 10
-        self.neurons_per_hidden = 80
-        self.features = nn.Sequential(
+    def __init__(self, sine, alpha=0.003, beta=0.005, gamma=0.003, decay=0.9, update_lambdas=False, use_schedule=True):
+        self.task = sine # task
+
+        # training details
+        self.lambdas = 1/sine.train_tasks * np.ones(sine.train_tasks) # initialize lambdas as uniform
+        self.gamma = gamma # step size for lambda update
+        self.update_lambdas = update_lambdas # whether ro update lambdas
+
+        # network details
+        self.embedding_dim = 10 # output dimension of embedding network
+        self.neurons_per_hidden = 80 # number of hidden neurons
+        self.features = nn.Sequential( # embedding network
             nn.Linear(1, self.neurons_per_hidden),
             nn.ReLU(),
             nn.Linear(self.neurons_per_hidden, self.neurons_per_hidden),
             nn.ReLU(),
-            # nn.Linear(40, 40),
-            # nn.ReLU(),
-            nn.Linear(self.neurons_per_hidden, self.embedding_dim) # (40, 10)
+            nn.Linear(self.neurons_per_hidden, self.embedding_dim)
         )
-        self.heads = [nn.Linear(self.embedding_dim, 1) for _ in range(sine.total_tasks)]
-        
+        self.heads = [nn.Linear(self.embedding_dim, 1) for _ in range(sine.total_tasks)] # task-specific network
+
+        # optimizers and schedulers
+        self.use_schedule = use_schedule # whether to use a scheduler
         self.feature_optimizer = torch.optim.SGD(list(self.features.parameters()), alpha)
-        # lambda1 = lambda epoch: 1/(epoch+1)**0.1
-        # self.feature_scheduler = torch.optim.lr_scheduler.StepLR(self.feature_optimizer, step_size=100, gamma=0.99)
-        # self.feature_scheduler = torch.optim.lr_scheduler.LambdaLR(self.feature_optimizer, lr_lambda=[lambda1])
-        
+        self.feature_scheduler = torch.optim.lr_scheduler.StepLR(self.feature_optimizer, step_size=100, gamma=decay)        
         self.head_optimizers = [torch.optim.Adam(list(self.heads[i].parameters()), beta) for i in range(sine.total_tasks)]
-        self.lambdas = 1/sine.train_tasks * np.ones(sine.train_tasks)
                 
     def test_full_batch(self, mode='train'):
+        """
+        Function to approximate the true loss of the current network
+        """
         if mode == 'train': task_list = np.arange(self.task.train_tasks)
         else: task_list = np.arange(self.task.test_tasks) + self.task.train_tasks
         losses = []
@@ -61,6 +55,9 @@ class Learner():
         return losses        
 
     def get_loss(self, task, xs, ys, reg=False, embedding=None):
+        """
+        Function to get the loss of the network from (x, y) pairs
+        """
         if embedding == None:
             embedding = self.features(xs)
 
@@ -78,6 +75,10 @@ class Learner():
         return total_loss
 
     def validation(self, task_list, n_shots):
+        """
+        meta-validation loop
+        """
+
         # clear all the gradients
         self.feature_optimizer.zero_grad()
         for a in task_list: self.head_optimizers[a].zero_grad()
@@ -90,14 +91,19 @@ class Learner():
         return losses
 
     def inner_loop(self, n_inner=2, n_shots=10, mode="train", verbose=True):
-        # sample list of task id's
+        """
+        inner loop for few-shot learning
+        samples n_shots samples from each task, then runs n_inner steps
+        """
+
+        # get the list of tasks (all tasks)
         if mode == "train": task_list = np.arange(self.task.train_tasks)
         elif mode == "test": task_list = np.arange(self.task.test_tasks) + self.task.train_tasks
 
         for task_id in task_list:             
             xs, ys = self.task.sample_batch(task_id, n_shots) # sample data
             embedding = self.features(xs) # outer loop
-            for it_inner in range(n_inner): # inner loop
+            for _ in range(n_inner): # inner loop
                 self.head_optimizers[task_id].zero_grad() # clear gradients
                 # print(list(self.heads[task_id].parameters())[0].detach().numpy().squeeze())
                 total_loss = self.get_loss(task_id, xs, ys, reg=True, embedding=embedding) # fwd
@@ -108,6 +114,9 @@ class Learner():
         return task_list
 
     def learn(self, n_outer=2000, n_inner=2, n_shots=10):
+        """
+        main loop
+        """
         all_avg_losses = []
         all_max_losses = []
         for it_outer in range(n_outer):
@@ -122,6 +131,10 @@ class Learner():
                 overall_loss += self.lambdas[t] * task_losses[i]
             overall_loss.backward()
             self.feature_optimizer.step()
+            if self.use_schedule:
+                self.feature_scheduler.step()
+                if it_outer % 100 == 0: 
+                    print(self.feature_scheduler.get_last_lr())
 
             if self.update_lambdas:
                 for i, t in enumerate(task_list):
@@ -129,9 +142,6 @@ class Learner():
                     # print("pull of task losses: ", task_losses[i].item()*self.gamma / ((it_outer + 1) ** (3/5)))
                     # print("pull of reg: ", -mu_lambda*(self.lambdas[t] - 1/self.task.train_tasks) * self.gamma / ((it_outer + 1) ** (3/5)))
                     self.lambdas[t] += (task_losses[i].item() - mu_lambda * (self.lambdas[t] - 1/self.task.train_tasks)) * self.gamma
-                    # self.lambdas[t] += (task_losses[i].item() - mu_lambda * (self.lambdas[t] - 1/self.task.train_tasks)) * self.gamma / ((it_outer + 1) ** (3/5))
-                    # self.lambdas[t] += task_losses[i].item() * self.gamma / ((it_outer + 1) ** 0.2)
-
                 self.lambdas = simplex_projection(self.lambdas)
 
             # log average loss for graphing purposes
@@ -146,16 +156,18 @@ class Learner():
                 # plt.title("max loss")
                 # plt.show()
             
-            # if it_outer % 100 == 0:
-            #     print(f"iteration {it_outer}")
-            #     print(f"losses!: {np.array([i.item() for i in task_losses])}")
-            #     print(f"true losses: {np.array(true_losses)}")
-            #     # print(f"tasks: {tasks)
-            #     print(f"lambdas: {self.lambdas[:50]}")
+            if it_outer % 100 == 0:
+                print(f"iteration {it_outer}")
+                print(f"     losses: {np.array([i.item() for i in task_losses])}")
+                print(f"true losses: {np.array(true_losses)}")
+                print(f"    lambdas: {self.lambdas[:50]}")
         
         return all_avg_losses, all_max_losses
 
     def evaluate(self):
+        """
+        testing loss (this function is not used)
+        """
         for i in range(5):
             self.inner_loop(mode="test", n_inner=2, verbose=False)
             
@@ -165,34 +177,73 @@ class Learner():
 
         return sum(true_losses) / len(true_losses), max(true_losses)
 
-import pickle
-from matplotlib import pyplot as plt
-
-def run_with_step_size(alpha=0.003, beta=0.005, gamma=0.003, n_outer=4000, n_inner=2, n_shots=10, run_id=0):
-    learner_lambdas = Learner(sine_task, alpha=alpha, beta=beta, gamma=gamma, update_lambdas=True)
-    loss_lambdas, max_loss_lambdas = learner_lambdas.learn(n_outer, n_inner, n_shots)
-
-    learner_no_lambdas = Learner(sine_task, alpha=alpha, beta=beta, gamma=gamma, update_lambdas=False)
-    loss_no_lambdas, max_loss_no_lambdas = learner_no_lambdas.learn(n_outer, n_inner, n_shots)
-
+train_tasks = 20
+test_tasks = 10
+sine_task = Sine(train_tasks, test_tasks)
+def run_with_step_size(lambdas=True, use_schedule=True, 
+                       alpha=0.003, beta=0.003, gamma=0.003, decay=0.9,
+                       n_outer=4000, n_inner=2, n_shots=10, run_id=-1):
     data = {}
-    data["avg_lambdas"] = loss_lambdas
-    data["max_lambdas"] = max_loss_lambdas
-    data["avg_no_lambdas"] = loss_no_lambdas
-    data["max_no_lambdas"] = max_loss_no_lambdas
-    with open(f"runs/{alpha}_{beta}_{gamma}_{run_id}.pkl", "wb") as f:
+    if lambdas:
+        learner_lambdas = Learner(sine_task, alpha=alpha, beta=beta, gamma=gamma, decay=decay, update_lambdas=True, use_schedule=use_schedule)
+        loss_lambdas, max_loss_lambdas = learner_lambdas.learn(n_outer, n_inner, n_shots)
+        data["lambdas"] = True
+        data["avg"] = loss_lambdas
+        data["max"] = max_loss_lambdas
+        # max_loss_lambdas = data["avg"]
+
+    else:
+        learner_no_lambdas = Learner(sine_task, alpha=alpha, beta=beta, gamma=gamma, update_lambdas=False, use_schedule=use_schedule)
+        loss_no_lambdas, max_loss_no_lambdas = learner_no_lambdas.learn(n_outer, n_inner, n_shots)
+        data["lambdas"] = False
+        data["avg"] = loss_no_lambdas
+        data["max"] = max_loss_no_lambdas
+        # max_loss_no_lambdas = data["max_no_lambdas"]
+
+    import glob
+    pkl_files = glob.glob("runs4/*.pkl")
+    run_nums = list(map(lambda x:int(x.split('_')[-1].split('.')[0]), pkl_files))
+    if run_id == -1:
+        if len(run_nums) == 0:
+            run_id = 1
+        else:
+            run_id = max(run_nums)+1
+    with open(f"runs4/{alpha}_{beta}_{gamma}_{lambdas}_{use_schedule}_{run_id}.pkl", "wb") as f:
         pickle.dump(data, f)
 
-    plt.clf()
-    plt.plot(range(len(max_loss_lambdas)), max_loss_lambdas, label='lambdas')
-    plt.plot(range(len(max_loss_lambdas)), max_loss_no_lambdas, label='no lambdas')
-    plt.legend()
-    plt.title("max loss")
-    plt.savefig(f"figs/{alpha}_{beta}_{gamma}_{run_id}.png")
+    return data["max"]
+    # plt.clf()
+    # plt.plot(range(len(max_loss_lambdas)), max_loss_lambdas, label='lambdas')
+    # plt.plot(range(len(max_loss_lambdas)), max_loss_no_lambdas, label='no lambdas')
+    # plt.legend()
+    # plt.title("max loss")
+    # plt.savefig(f"figs2/{alpha}_{beta}_{gamma}_{run_id}.png")
 
+# run_id = 100
+# for runs in range(50):
+#     for alpha in [0.02]: # 0.007, 0.01, 0.013, 0.016]:
+#         for beta in [0.005, 0.007, 0.009, 0.011]:
+#             for gamma in [0.003]:
+
+loss_sched = run_with_step_size(lambdas=True, use_schedule=True, 
+                            alpha=0.02, beta=0.01, gamma=0.003, decay=1,
+                            n_outer=6000, n_inner=3, n_shots=15, run_id=-1)
+quit()            
+losses_all = []
 run_id = 0
-for alpha in [0.003]:
-    for beta in [0.005]:
-        for gamma in [0.003]:
-            run_with_step_size(alpha, beta, gamma, 10000, 2, 10, run_id)
-            run_id += 1
+for i in range(1):
+    for alpha in [0.005, 0.01, 0.015, 0.02]:
+        for beta in [0.002, 0.004, 0.007, 0.01]:
+            for decay in [0.96, 0.92, 0.88]:
+                gamma = 0.003
+                for lambdas in [True, False]:
+                    run_id += 1
+                    print(alpha, beta, gamma, lambdas, True, run_id)
+                    try:
+                        a = 1
+                        loss_sched = run_with_step_size(lambdas=lambdas, use_schedule=True, 
+                                                    alpha=alpha, beta=beta, gamma=gamma, decay=decay,
+                                                    n_outer=6000, n_inner=3, n_shots=15, run_id=-1)
+                    except:
+                        pass
+                
